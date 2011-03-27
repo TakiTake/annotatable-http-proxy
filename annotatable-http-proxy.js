d6c4b7a67e1ff365d0cb292a99b9f818828ae5e4
TakiTake <TakiTake.create@gmail.com>
2011-03-27 12:29:12 +0900

    refactoring

--- annotatable-http-proxy.js
+++ annotatable-http-proxy.js
@@ -2,25 +2,31 @@
 
 var http = require('http');
 var url  = require('url');
+var util = require('util');
 var server = http.createServer();
 
-server.on('request', requestListener);
-server.listen(8000);
+server
+	.on('request', requestListener)
+	.listen(8000);
+
+var DEBUG = false;
+function log(str) {
+	DEBUG && util.puts(str);
+}
 
 function requestListener(req, res) {
+	log(req.url);
 	var requestUrl = url.parse(req.url);
-	var client = http.createClient(requestUrl.port || 80, requestUrl.host);
-	var clientRequest = client.request(req.url, req.headers);
+	var port = getPort(requestUrl.port, requestUrl.protocol);
+	var client = http.createClient(port, requestUrl.host);
 	var callbackSent = false;
 	var contents = [];
-
-	if (requestUrl.protocol === 'https') {
-		client.https = true;
-	}
+	var clientRequest = client.request(req.url, req.headers);
 
 	clientRequest.on('response', onResponse);
 	clientRequest.on('error', onError);
 	clientRequest.on('close', onClose);
+	clientRequest.end();
 
 	function onResponse(clientResponse) {
 		var statusCode = clientResponse.statusCode;
@@ -30,20 +36,12 @@ function requestListener(req, res) {
 		// console.log('[' + new Date() + '] ' + '"' + req.method + ' ' + req.url + '" '
 		// 		+ statusCode + ' "' + req.headers['user-agent'] + '"');
 
+		res.writeHead(statusCode, clientResponse.headers);
 		clientResponse.setEncoding(enc);
-		if (statusCode < 200 || 300 <= statusCode) {
-			if (!callbackSent) {
-				res.writeHead(statusCode, clientResponse.headers);
 
-				if (clientResponse.method === 'HEAD' || statusCode === 304) {
-					res.end('');
-				}
-				else {
-					res.end(statusCode, enc);
-				}
-
-				callbackSent = true;
-			}
+		if (statusCode === 304 || clientResponse.method === 'HEAD') {
+			res.end('');
+			callbackSent = true;
 			client.end();
 
 			return;
@@ -58,21 +56,14 @@ function requestListener(req, res) {
 
 		function onEnd() {
 			if (!callbackSent) {
-				res.writeHead(statusCode, clientResponse.headers);
-
-				if (clientResponse.method === 'HEAD') {
-					res.end('');
-				}
-				else {
-					res.end(contents.join(''), enc);
-				}
-
+				res.end(contents.join(''), enc);
 				callbackSent = true;
 			}
 		}
 	}
 
 	function onError(e) {
+		log('error');
 		if (!callbackSent) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.end(e.message, 'utf8');
@@ -81,12 +72,27 @@ function requestListener(req, res) {
 	}
 
 	function onClose() {
+		log('closed');
 		if (!callbackSent) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.end('Connection closed unexpectedly', 'utf8');
 			callbackSent = true;
 		}
 	}
+}
 
-	clientRequest.end();
+function getPort(port, protocol) {
+	if (!port) {
+		switch (protocol) {
+			case 'https':
+				port = 443;
+				break;
+
+			case 'http':
+			default:
+				port = 80;
+		}
+	}
+
+	return port;
 }
