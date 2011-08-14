005e15ac86cfde155ec587c9384428c716651ad8
TakiTake <TakiTake.create@gmail.com>
2011-08-14 09:10:08 +0900

    tmp

--- annotatable-http-proxy.js
+++ annotatable-http-proxy.js
@@ -4,20 +4,29 @@ var fs   = require('fs');
 var http = require('http');
 var url  = require('url');
 var util = require('util');
-var CONF_FILE_PATH = './etc/conf.json';
+var jsdom     = require('jsdom');
+var domToHtml = require('jsdom/lib/jsdom/browser/domtohtml').domToHtml;
 
+const CONF_FILE_PATH = './etc/conf.json';
 var conf = null;
 var server = http.createServer()
 
-function log(str) {
-	conf.debug && util.puts(str);
+function log(opt) {}
+log.debug = function() {
+	console.log.apply(this, arguments);
+}
+log.info = function() {
+	console.info.apply(this, arguments);
+}
+log.error = function() {
+	console.error.apply(this, arguments);
 }
 
-function reload(filename, sync) {
+function reload(filepath, sync) {
 	if (sync) {
-		conf = JSON.parse(fs.readFileSync(filename, 'utf8'));
+		conf = JSON.parse(fs.readFileSync(filepath, 'utf8'));
 	} else {
-		fs.readFile(filename, 'utf8', function(err, data) {
+		fs.readFile(filepath, 'utf8', function(err, data) {
 			if (err) {
 				throw err;
 			}
@@ -29,7 +38,7 @@ function reload(filename, sync) {
 
 // TODO: close request if blowser closed
 function requestListener(req, res) {
-	log(req.url);
+	log.debug(req.url);
 	var requestUrl = url.parse(req.url);
 	var port = getPort(requestUrl.port, requestUrl.protocol);
 	var client = http.createClient(port, requestUrl.host);
@@ -44,7 +53,8 @@ function requestListener(req, res) {
 
 	function onResponse(clientResponse) {
 		var statusCode = clientResponse.statusCode;
-		var enc = /text/.test(clientResponse.headers['contents-type']) ? 'utf8' : 'binary';
+		var contentType = clientResponse.headers['content-type'];
+		var enc = /text/.test(contentType) ? 'utf8' : 'binary';
 
 		// access log
 		// log('[' + new Date() + '] ' + '"' + req.method + ' ' + req.url + '" '
@@ -70,14 +80,31 @@ function requestListener(req, res) {
 
 		function onEnd() {
 			if (!callbackSent) {
-				res.end(contents.join(''), enc);
+				var html = contents.join(''); 
+
+				// TODO:cache
+				// TODO:tidy
+				// TODO:iconv
+				if (/text\/html/.test(contentType)) {
+					jsdom.env(html, [
+						//'http://code.jquery.com/jquery-1.5.min.js'
+					], function(errors, window) {
+						if (errors) {
+							throw errors;
+						} else {
+							html = window.document.doctype || '';
+							html += domToHtml(window.document, true);
+						}
+					});
+				}
+				res.end(html, enc);
 				callbackSent = true;
 			}
 		}
 	}
 
 	function onError(e) {
-		log('error');
+		log.debug('error');
 		if (!callbackSent) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.end(e.message, 'utf8');
@@ -86,7 +113,7 @@ function requestListener(req, res) {
 	}
 
 	function onClose() {
-		log('closed');
+		log.debug('closed');
 		if (!callbackSent) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.end('Connection closed unexpectedly', 'utf8');
@@ -123,7 +150,7 @@ function init() {
 	server.on('request', requestListener)
 		    .listen(conf.port);
 	
-	log("Listening " + conf.port);
+	log.info("Listening " + conf.port);
 }
 
 init();
